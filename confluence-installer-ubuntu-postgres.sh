433b4367d7d68ce24c00d6cf8393577399a67376
Joshua Carpenter <mtnbikerjoshua@gmail.com>
2018-08-24 14:44:46 -0600

    Update confluence-installer-ubuntu-postgres.sh

--- confluence-installer-ubuntu-postgres.sh
+++ confluence-installer-ubuntu-postgres.sh
@@ -2,7 +2,7 @@ apt-get update
 apt-get install postgresql-10
 runuser -l postgres -c "createuser confluenceuser"
 runuser -l postgres -c "createdb -O confluenceuser confluencedb"
-runuser -l postgres -c "psql -U postgres -c \"ALTER USER confluenceuser WITH ENCRYPTED PASSWORD '69j50Ae@'\""
+runuser -l postgres -c "psql -U postgres -c \"ALTER USER confluenceuser WITH ENCRYPTED PASSWORD 'typepassword'\""
 echo "Downloading Confluence installer..."
 wget "https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-6.10.0-x64.bin"
 sudo chmod 555 atlassian-confluence-6.10.0-x64.bin
